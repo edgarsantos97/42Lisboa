b9daff79290cbb688f84c7c119f9ef1734a94a97
edgarsantos97 <edgarsantos97@hotmail.com>
2024-11-08 17:35:41 +0000

    ft_isalpha done

--- 42cursus/libft/ft_isalpha.c
+++ 42cursus/libft/ft_isalpha.c
@@ -3,14 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edgarrod <edgarrod@student.42.fr>          +#+  +:+       +#+        */
+/*   By: edgarrod <edgarrod@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/03 19:59:05 by edgarrod          #+#    #+#             */
-/*   Updated: 2024/11/03 19:59:27 by edgarrod         ###   ########.fr       */
+/*   Created: 2024/11/08 16:20:36 by edgarrod          #+#    #+#             */
+/*   Updated: 2024/11/08 17:34:51 by edgarrod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-ft_isalpha()
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int ft_isalpha(int c)
 {
-    
+    if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
+/*
+int main()
+{
+    char c;
+
+    c = 'A';
+    
+    printf("ft_isalpha: %d", ft_isalpha(c));
+    printf("\n");
+    printf("isalpha: %d", isalpha(c));
+}
+*/
