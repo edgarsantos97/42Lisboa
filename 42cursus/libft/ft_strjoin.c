513fb1ab4cc18ef9dc2b1e2e70297aba949df45a
edgarsantos97 <edgarsantos97@hotmail.com>
2024-11-23 19:16:33 +0000

    done

--- 42cursus/libft/ft_strjoin.c
+++ 42cursus/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: edgarrod <edgarrod@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 14:31:06 by edgarrod          #+#    #+#             */
-/*   Updated: 2024/11/23 16:20:56 by edgarrod         ###   ########.fr       */
+/*   Updated: 2024/11/23 17:59:00 by edgarrod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,27 +32,23 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	while (s1[l] != '\0')
 	{
-		str[l] = s1[i];
+		str[l] = s1[l];
 		l++;
 	}
-	n = 0;
-	while (s2[n] != '\0')
+	while (l < (i + n))
 	{
-		str[l] = s2[n];
+		str[l] = s2[l - i];
 		l++;
-		n++;
 	}
 	str[l] = '\0';
 	return (str);
 }
 
-int	main(void)
+/* int	main(void)
 {
 	char *s1 = "Hello";
 	char *s2 = "World!";
-	
 
 	char *str = ft_strjoin(s1, s2);
 	printf("ft_strjoin: |%s", str);
-
-} 
+} */
--- 42cursus/libft/ft_strtrim.c
+++ 42cursus/libft/ft_strtrim.c
@@ -6,20 +6,79 @@
 /*   By: edgarrod <edgarrod@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 21:41:09 by edgarrod          #+#    #+#             */
-/*   Updated: 2024/11/23 16:03:38 by edgarrod         ###   ########.fr       */
+/*   Updated: 2024/11/23 19:11:50 by edgarrod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
-/* char *ft_strtrim(char const *s1, char const *set)
+int	ft_char(char c, char const *set)
 {
-    char *
+	size_t	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (c == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-int main()
+int	ft_charstart(char const *s, char const *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && ft_char(s[i], set) == 1)
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	ft_charend(char const *s, char const *set, size_t slen)
+{
+    while (slen > 0 && ft_char(s[slen - 1], set) == 1)
+    {
+        slen--;
+    }
+    return (slen);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+    size_t s;
+    size_t e;
+    char *strim;
+    
+    s = ft_charstart(s1, set);
+    e = ft_charend(s1, set, ft_strlen(s1));
+    if (s >= e)
+    {
+        strim = malloc(1);
+        if (!strim)
+            return NULL;
+        strim[0] = '\0';
+        return strim;
+    }
+    strim = malloc(sizeof(char) * (e - s + 1));
+    if(!strim)
+        return NULL;
+    ft_strlcpy(strim, s1 + s, (e - s + 1));
+    return (strim);
+}
+
+/* int	main(void)
 {
     
+    char *trim = ft_strtrim("abcHelloWorldabc", "abc");
+    printf("%s\n", trim);
+    trim = ft_strtrim("", "");
+    printf("%s\n", trim);
+    trim = ft_strtrim("", "abc");
+    printf("%s\n", trim);
 } */
