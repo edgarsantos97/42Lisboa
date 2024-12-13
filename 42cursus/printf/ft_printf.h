c40337d15ee11c94ef543ad7fdf216ff90d558f6
edgarsantos97 <edgarsantos97@hotmail.com>
2024-12-13 15:43:11 +0000

    done

---
+++ 42cursus/printf/ft_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edgarrod <edgarrod@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 15:38:20 by edgarrod          #+#    #+#             */
+/*   Updated: 2024/12/13 15:42:28 by edgarrod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
